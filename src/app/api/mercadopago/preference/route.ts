0940dd07d94d3dd094b93245f2981f413cf2fd67
ALDOINMO <aldo.f.fabricatore@gmail.com>
2026-05-28 17:33:59 -0300

    update production url

--- src/app/api/mercadopago/preference/route.ts
+++ src/app/api/mercadopago/preference/route.ts
@@ -88,7 +88,7 @@ export async function POST() {
             user.realEstateId,
 
           notification_url:
-            "https://inmomatch-41hf.vercel.app/api/mercadopago/webhook",
+             "https://inmomatch-lilac.vercel.app/api/mercadopago/webhook",
         },
       });
 
